4452c7f20f8a0ac6d2cc2f45547202b65ae14252
Eetu <99178022+E3TU@users.noreply.github.com>
2024-06-14 00:20:36 +0300

    Initialized the project

---
+++ main.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+
+// let url = "https://youtube.com/watch?v=XW6-00SqPHM";
+
+// ytdl(url, {filter: "audioonly", quality: "highest" })
+//     .pipe(fs.createWriteStream("video.mp3"));
